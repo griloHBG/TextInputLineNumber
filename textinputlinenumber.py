fd252a773a79bb04452b909d2c08224d0bb53c0d
henrique.garcia <henrique.garcia@toradex.com>
2021-07-08 01:03:22 -0300

    Exposing some useful properties and methods

--- textinputlinenumber.py
+++ textinputlinenumber.py
@@ -1,10 +1,12 @@
 from kivy.clock import Clock
-from kivy.properties import ObjectProperty, NumericProperty, ListProperty, StringProperty
+from kivy.properties import ObjectProperty, NumericProperty, ListProperty, StringProperty, AliasProperty
+from kivy.uix.behaviors import FocusBehavior
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.codeinput import CodeInput
 from kivy.uix.textinput import TextInput
 
 
-class LineNumbers(TextInput):
+class LineNumbers(CodeInput):
     lines_flags = ListProperty([])
 
     def on_lines_flags(self, *args):
@@ -25,9 +27,10 @@ class LineNumbers(TextInput):
         self._update_graphics()
 
 
-class CustomTextInput(TextInput):
+class CustomTextInput(CodeInput):
     amount_lines = NumericProperty(1)
     lines_flags = ListProperty([])
+    cursor_line = NumericProperty(1)
 
     LINE_FLAG_WORDWRAP = 0
     LINE_FLAG_NEWLINE = 1
@@ -45,8 +48,24 @@ class CustomTextInput(TextInput):
     def on_scroll_y(self, *args):
         self._update_graphics()
 
+    def go_to_line(self, line_number):
+        print(self.cursor, 'começo')
+        self.cursor = (0,0)
+        self.cursor_line = 1
+        flag_idx = 0
+        while self.cursor_line < line_number-1:
+            if self.lines_flags[flag_idx] == self.LINE_FLAG_NEWLINE:
+                self.cursor_line+=1
+            # print(self.cursor, flag_idx, "new line" if self.lines_flags[flag_idx] == self.LINE_FLAG_NEWLINE else "no new line")
+            flag_idx+=1
+        #TODO: sometimes we get 1 line before the intended line
+        # print('linha', self.cursor_line, line_number)
+        if not self.lines_flags[flag_idx] == self.LINE_FLAG_NEWLINE:
+            flag_idx+=1
+        self.cursor = (0, flag_idx)
 
-class TextInputLineNumber(BoxLayout):
+
+class TextInputLineNumber(FocusBehavior, BoxLayout):
     text_content = ObjectProperty(None)
     line_numbers = ObjectProperty(None)
 
@@ -55,9 +74,31 @@ class TextInputLineNumber(BoxLayout):
     text = StringProperty('')
     scroll_y = NumericProperty(0)
 
+    selection = StringProperty('')
+
+    cursor_line = NumericProperty(1)
+
     def update_graphics(self):
         self.line_numbers._update_graphics()
         self.text_content._update_graphics()
 
     def do_cursor_movement(self, action, control=False, alt=False):
         self.text_content.do_cursor_movement(action, control, alt)
+
+    def cursor_index(self, cursor=None):
+        return self.text_content.cursor_index(cursor)
+
+    def go_to_line(self, line_number):
+        self.text_content.go_to_line(line_number)
+
+    def get_current_line(self):
+        return self.text_content.cursor_line
+
+    def select_text(self, start, end):
+        self.text_content.select_text(start, end)
+
+    def get_cursor_from_xy(self, x, y):
+        return self.text_content.get_cursor_from_xy(x, y)
+
+    def get_cursor_from_index(self, text_index):
+        return self.text_content.get_cursor_from_index(text_index)
